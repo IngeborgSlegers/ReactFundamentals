cf25a07c89133095779664c9dc57c4111a7be561
[IngeborgSlegers] <[ingeborgfreda@gmail.com]>
2019-02-12 10:37:07 -0500

    JSX Rules

--- src/components/concepts/JSXRules.js
+++ src/components/concepts/JSXRules.js
@@ -4,14 +4,14 @@ const JSXRules = () => {
   return (
     <div className="main">
       <div className="maindiv">
-      <dl>
-      <dt>Resembles HTML</dt>
-      <dd>
-        It's not HTML. Closer to JS.
-      </dd>
-      </dl>
-React.createElement() - Used to construct and update the DOM.
-Not required - Developers don't have to use JSX. 
+        <dl>
+          <dt>Resembles HTML</dt>
+          <dd>It's not HTML. Closer to JS.</dd>
+          <dt>React.createElement()</dt>
+          <dd>Used to construct and update the DOM</dd>
+          <dt>Not Required</dt>
+          <dd>Developers don't have to use JSX</dd>
+        </dl>
       </div>
     </div>
   )
